8d10fe54e49bce2f6fea74f992464e7f2eec158c
Janmejay Rai <janmejay.rai31@gmail.com>
2020-11-12 11:57:32 +0530

    Adding Procfile for heroku deployment

--- config/puma.rb
+++ config/puma.rb
@@ -7,6 +7,7 @@
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
@@ -18,6 +19,14 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+
+  if defined?(Resque)
+     Resque.redis = ENV["<redis-uri>"] || "redis://127.0.0.1:6379"
+  end
+end
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
@@ -32,7 +41,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-# preload_app!
+preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
